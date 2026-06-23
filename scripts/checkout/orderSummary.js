10a5673c81847155f32a78499b046e9c9c670ac8
Ujjwal Singh <ujjwalkumr05@gmail.com>
2026-06-23 12:38:47 +0530

    Extract delivery date calculation into reusable function

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -1,16 +1,14 @@
 import { cart, removeFromCart, calculateCartQuantity, updateQuantity, updateDeliveryOption } from "../../data/cart.js";
 import { products, getProduct } from "../../data/products.js";
 import { formatCurrency } from "../utils/money.js";
-import { deliveryOptions, getDeliveryOption } from "../../data/deliveryOption.js";
+import { deliveryOptions, getDeliveryOption, calculateDeliveryDate} from "../../data/deliveryOption.js";
 import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
 import { renderPaymentSummary } from "./paymentSummary.js";
 import {renderCheckoutHeader} from "./checkoutHeader.js";
 
 
 
-
 export function renderOrderSummary() {
-    // renderCheckoutHeader();
   let cartSummaryHTML = '';
   cart.forEach((cartItem) => {
     let productId = cartItem.productId;
@@ -19,16 +17,8 @@ export function renderOrderSummary() {
 
     const deliveryOption = getDeliveryOption(deliveryOptionId);
 
-    const today = dayjs();
-
-    const deliveryDate = today.add(
-      deliveryOption.deliveryDays,
-      'days'
-    );
-    ;
-    const dateString = deliveryDate.format(
-      'dddd, MMMM D'
-    );
+    
+    const dateString = calculateDeliveryDate(deliveryOption);
 
 
 
@@ -84,16 +74,8 @@ export function renderOrderSummary() {
     let html = '';
 
     deliveryOptions.forEach((deliveryOption) => {
-      const today = dayjs();
-
-      const deliveryDate = today.add(
-        deliveryOption.deliveryDays,
-        'days'
-      );
-
-      const dateString = deliveryDate.format(
-        'dddd, MMMM D'
-      );
+      
+      const dateString = calculateDeliveryDate(deliveryOption);
       const priceString = deliveryOption.priceCents === 0
         ? 'FREE'
         : `${formatCurrency(deliveryOption.priceCents)}`
